56ede0a2ffd663cbb55e82dc36d84fb81a31b7b6
Vadim Prokoshin <vpro85@gmail.com>
2021-09-05 23:23:54 +0200

    Add DrawTriangles to utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -2642,11 +2642,36 @@ export const TRIANGULATION = [
     255,
 ];
 //Draw triangle
-//Draw the points
+const drawPath = (ctx, points, closePath) => {
+    const region = new Path2D();
+    region.moveTo(points[0][0], points[0][1])
+    for (let i = 1; i < points.length; i++) {
+        const point = points[i];
+        region.lineTo(point[0], point[1])
+    }
+    if (closePath) {
+        region.closePath();
+    }
+    ctx.strokeStyle = "gray";
+    ctx.stroke(region);
+}
+
+//Draw Mesh
 export const drawMesh = (predictions, ctx) => {
     if (predictions.length > 0) {
         predictions.forEach(prediction => {
             const keypoints = prediction.scaledMesh;
+
+            //Draw triangles
+            for (let i = 0; i < TRIANGULATION.length / 3; i++) {
+                const points = [
+                    TRIANGULATION[i * 3],
+                    TRIANGULATION[i * 3 + 1],
+                    TRIANGULATION[i * 3 + 2],
+                ].map((index) => keypoints[index])
+                drawPath(ctx, points, true)
+            }
+            //Draw points
             for (let i = 0; i < keypoints.length; i++) {
                 const x = keypoints[i][0]
                 const y = keypoints[i][1]
